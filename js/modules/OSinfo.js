50bd40f1847964ebae185110a89ac49db3e6d0bf
Tomasz Lis <tomasz.lis@o2.pl>
2018-09-24 23:07:19 +0200

    Add files of exercise 13.4
    *Add basic code
    *Add folder "app" and files "program.js"
    *Add filder "modules" and file "OSinfo.js"

---
+++ js/modules/OSinfo.js
@@ -0,0 +1,25 @@
+'use scrict';
+
+// Node.js - Common JS Modules
+var os = require('os');
+
+function getOSinfo() {
+	var type = os.type();
+	if (type === 'Darwin') {
+		type = 'OSX';
+	} else if (type === 'Windows_NT'); {
+		type = 'Windows';
+	}
+	var release = os.release();
+	var cpu = os.cpus()[0].model;
+	var uptime = os.uptime();
+	var userInfo = os.userInfo();
+	console.log('System:', type);
+	console.log('Release:', release);
+	console.log('CPU Model:', cpu);
+	console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
+	console.log('User name:', userInfo.username);
+	console.log('Home dir:', userInfo.homedir);
+};
+
+exports.print = getOSinfo;
